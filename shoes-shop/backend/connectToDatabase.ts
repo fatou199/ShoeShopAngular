79b954de558a26c95c9f7d15033e570af0edc63e
kone_f <kone_f@etna-alternance.net>
2024-12-22 03:26:13 +0100

    connexion bdd avec mongoose

---
+++ shoes-shop/backend/connectToDatabase.ts
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const dbHost = process.env.DB_HOST
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASSWORD
+const dbName = process.env.DB_NAME
+
+// en local
+//const uri = `${dbHost}://${dbUser}:${dbPassword}@localhost:27017/${dbName}`;
+
+// dans docker
+const uri = `${dbHost}://${dbUser}:${dbPassword}@${dbHost}:27017/${dbName}?authSource=admin`;
+
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(uri);
+    console.log('Connexion à MongoDB réussie !');
+  } catch (error: any) {
+    console.error('Erreur de connexion à MongoDB:', error.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
--- shoes-shop/backend/src/index.ts
+++ shoes-shop/backend/src/index.ts
@@ -1,13 +1,26 @@
 import express, { Request, Response } from 'express';
 require('dotenv').config()
+import connectDB from '../connectToDatabase';
 
 const app = express()
 const port = process.env.PORT
 
-app.get('/', (req, res) => {
-    res.send('Hello world')
-  })
 
-app.listen(port, () => {
-  console.log(`Serveur démarré sur http://localhost:${port}`);
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello world')
 })
+
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(port, () => {
+      console.log(`Serveur démarré sur http://localhost:${port}`);
+    });
+  } catch (error) {
+    console.error('Erreur de démarrage du serveur:', error);
+    process.exit(1);
+  }
+};
+
+startServer()
