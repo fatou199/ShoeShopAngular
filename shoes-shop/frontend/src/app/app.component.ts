7c91cfba945cc618d26cbd79e0216903840c40f4
fatou199 <fatou.kone99@outlook.fr>
2024-12-30 00:18:42 +0100

    delete line import in app.component

--- shoes-shop/frontend/src/app/app.component.ts
+++ shoes-shop/frontend/src/app/app.component.ts
@@ -5,7 +5,6 @@ import { ApiService } from './services/api.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
