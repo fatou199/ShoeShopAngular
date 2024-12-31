d763a1fceaafeb6d3217a8af6b021fcb8ecca504
fatou199 <fatou.kone99@outlook.fr>
2025-01-01 00:08:25 +0100

    ajout de log et modification de la fonction

--- shoes-shop/backend/src/middleware/auth.middleware.ts
+++ shoes-shop/backend/src/middleware/auth.middleware.ts
@@ -1,23 +1,37 @@
 const jwt = require('jsonwebtoken')
 import { NextFunction, Request, Response } from 'express';
+import User from '../models/User';
 
 
 
-const verifyToken = (req: Request & {userId?: any}, res: Response, next: NextFunction) => {
+const verifyToken = async (req: Request & { user?: any }, res: Response, next: NextFunction) => {
     const token = req.headers.authorization?.split(' ')[1];
-   
+    // console.log('Token reçu:', token); 
+
+
     if (!token) {
-     return res.status(401).json({ message: 'Accès non autorisé : pas de token fourni.' });
+        // console.log('Aucun token trouvé dans l\'en-tête Authorization');
+        return res.status(401).json({ message: 'Accès non autorisé : pas de token fourni.' });
     }
-   
+
     try {
-     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-     req.userId = decoded.userId; // Injecte userId dans la requête
-     next();
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        // console.log('Token décodé:', decoded);
+
+
+        const user = await User.findById(decoded.userId);
+        if (!user) {
+            return res.status(404).json({ message: 'Utilisateur non trouvé' });
+        }
+
+        req.user = user;
+        // console.log('Utilisateur ajouté à la requête:', req.user);
+        next();
     } catch (err) {
-     return res.status(401).json({ message: 'Token invalide ou expiré.' });
+        // console.log('Erreur de vérification du token:', error);
+        return res.status(401).json({ message: 'Token invalide ou expiré.' });
     }
-   };
+};
 
 
-   export default verifyToken;
+export default verifyToken;
